cde542e144742fb9a67f3aafdb737a65abc4f2aa
Gorka <gorka1802@gmail.com>
2018-03-31 20:37:58 +0200

    pwd

---
+++ src/Controller/Frontend/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        die("HASD")
+    }
+}
