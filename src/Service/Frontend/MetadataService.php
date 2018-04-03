f274f0ad7bff64708913467449f1d6159124a9e0
Gorka Muñoz <gorka.munoz@acilia.es>
2018-04-03 11:21:30 +0200

    [bug] refactorized metadata

--- src/Service/Frontend/MetadataService.php
+++ src/Service/Frontend/MetadataService.php
@@ -11,34 +11,52 @@ class MetadataService
     public function getMetadata($data)
     {
         $data = json_decode($data, true);
+
         foreach($data as $key => $item) {
             if(is_array($data[$key])){
-                if($key == 'ldjson') {
-                    $this->getLdJsonMetaTags($item);
-                }
-
-                if($key == 'og') {
-                    $this->getOpenGraphMetaTags($item);
-                }
-
+                $this->getObjectMetaTag($key, $item);
             }else{
-                switch($key) {
-                    case 'title':
-                        echo '<title>' . $item . '</title>';
-                        break;
-
-                    case 'description':
-                        echo '<meta name="' . $key . '" content="' . $item . '">';
-                        break;
-                }
+                $this->getSingleMetaTag($key, $item);
             }
+        }
+    }
+
+    /**
+     * @param $key
+     * @param $item
+     */
+    private function getObjectMetaTag($key, $item)
+    {
+        if($key == 'ldjson') {
+            $this->getLdJsonMetaTags($item);
+        }
+
+        if($key == 'og') {
+            $this->getOpenGraphMetaTags($item);
+        }
+    }
+
+    /**
+     * @param $key
+     * @param $item
+     */
+    private function getSingleMetaTag($key, $item)
+    {
+        switch($key) {
+            case 'title':
+                echo '<title>' . $item . '</title>';
+                break;
+
+            case 'description':
+                echo '<meta name="' . $key . '" content="' . $item . '">';
+                break;
         }
     }
 
     /**
      * @param $item
      */
-    public function getLdJsonMetaTags($item)
+    private function getLdJsonMetaTags($item)
     {
         echo '<script type="application/ld+json">' . json_encode($item) . '</script>';
     }
@@ -48,7 +66,7 @@ class MetadataService
      * @return mixed
      * @internal param $limit
      */
-    public function getOpenGraphMetaTags(array $data)
+    private function getOpenGraphMetaTags(array $data)
     {
         foreach($data as $key => $item) {
             echo '<meta property="og:' . $key. '" content="' . $item . '">';
